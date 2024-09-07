55026e7a7503048848c7ab946689f31587a7aafa
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-09-07 14:01:48 +0200

    Fix linter issues

--- node_helper.js
+++ node_helper.js
@@ -1,24 +1,24 @@
-const NodeHelper = require("node_helper");
+const NodeHelper = require("node_helper")
 
 module.exports = NodeHelper.create({
 
-    async socketNotificationReceived(notification, payload) {
-        const self = this;
+  async socketNotificationReceived(notification, payload) {
+    const self = this
 
-        if (notification === "GET_QUIZ_QUESTIONS") {
-            let url = `https://the-trivia-api.com/v2/questions?limit=${payload.limit || 1}`
-            if (payload.difficulties)
-                url += `&difficulties=${payload.difficulties}`
-            if (payload.tags)
-                url += `&tags=${payload.tags}`
+    if (notification === "GET_QUIZ_QUESTIONS") {
+      let url = `https://the-trivia-api.com/v2/questions?limit=${payload.limit || 1}`
+      if (payload.difficulties)
+        url += `&difficulties=${payload.difficulties}`
+      if (payload.tags)
+        url += `&tags=${payload.tags}`
 
-            try {
-                const response = await fetch(url)
-                const body = await response.json()
-                self.sendSocketNotification("QUIZ_QUESTIONS", body)
-            } catch (error) {
-                self.sendSocketNotification("QUIZ_QUESTIONS_FAILED", error)
-            }
-        }
-    },
-});
+      try {
+        const response = await fetch(url)
+        const body = await response.json()
+        self.sendSocketNotification("QUIZ_QUESTIONS", body)
+      } catch (error) {
+        self.sendSocketNotification("QUIZ_QUESTIONS_FAILED", error)
+      }
+    }
+  },
+})
